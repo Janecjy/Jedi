0b93e49cbd3eed1589c003cc73cb6e1cbf055685
Jane Chen <janechen@cs.wisc.edu>
2023-06-01 00:51:45 -0700

    check existing varsize traces

--- scripts/genTraceVarSize.sh
+++ scripts/genTraceVarSize.sh
@@ -9,8 +9,8 @@ do
     for f in $FILES
     do
         NAME=$(basename "$f")  # Extract the filename with extension
-        if [ ! -e "/mydata/traces-"$r"x/"${NAME%.*}".txt" ] && ! pgrep -f $NAME && [[ $(wc -l < $f) -eq 100000000 ]]; then
-
+        if [[ ! -e "/mydata/traces-"$r"x/"${NAME%.*}".txt" || ! $(wc -l < "/mydata/traces-"$r"x/"${NAME%.*}".txt") -eq 100000000 ]] && ! pgrep -f $NAME && [[ $(wc -l < $f) -eq 100000000 ]]; then
+            rm "/mydata/traces-"$r"x/"${NAME%.*}".txt"
             python3 scripts/genTraceVarSize.py $r $f "/mydata/traces-"$r"x/"${NAME%.*}".txt" &
             ((COUNT++))
             if [ $COUNT -eq $COUNTMAX ]
