ca68d08829b78f0cb2323f58978b9a225632b5c0
Jane Chen <janechen@cs.wisc.edu>
2023-06-24 00:15:29 -0700

    genVarSizeJob scripts

--- scripts/tacc_scripts/genVarSizeJob.sh
+++ scripts/tacc_scripts/genVarSizeJob.sh
@@ -9,7 +9,6 @@ do
     do
         NAME=$(basename "$f")  # Extract the filename with extension
         if [[ ! -e $BASE_DIR"traces-"$r"x/"${NAME%.*}".txt" || ! $(wc -l < $BASE_DIR"traces-"$r"x/"${NAME%.*}".txt") -eq 100000000 ]] && ! pgrep -f $NAME && [[ $(wc -l < $f) -eq 100000000 ]]; then
-            rm "/mydata/traces-"$r"x/"${NAME%.*}".txt"
             echo "python3 scripts/genTraceVarSizeNoise.py "$r" "$f" "$BASE_DIR"traces-"$r"x/"${NAME%.*}".txt" &
         fi
     done
